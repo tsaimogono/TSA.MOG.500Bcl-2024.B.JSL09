2c2fe974c57ce390fd404156aec00a017286c861
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-04-09 15:18:41 +0200

    fetching data about Dogecoin from the CoinGecko API,

--- index.js
+++ index.js
@@ -23,3 +23,18 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `
+        
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+setInterval(getCurrentTime, 1000)
